39ae2a32e44db340dd5d8d8b7c625780191a9c1b
Marc Hibbins <marchibbins@gmail.com>
2016-08-20 10:38:24 +0000

    Render template for Hello World

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,6 +10,16 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+        'DIRS': [
+            os.path.join(BASE_DIR, 'mysite', 'templates'),
+        ]
+    },
+]
+
 ROOT_URLCONF = 'mysite.urls'
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
--- mysite/views.py
+++ mysite/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    return render(request, 'index.html', {
+        'title': 'Hello world',
+    })
